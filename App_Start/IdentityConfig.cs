6e472b5587ee9ca31b6db1cc6c1534d439561657
Jinisha Christopher <jchristopher@as-software.com>
2023-02-28 22:19:07 -0500

    Inital Commit

---
+++ App_Start/IdentityConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+//using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Helpers;
+using ITfoxtec.Identity.Saml2;
+using ITfoxtec.Identity.Saml2.Util;
+using System.IdentityModel.Claims;
+using System.ServiceModel.Security;
+
+
+namespace ASOnlineSAMLWebApp.App_Start
+{
+    public class IdentityConfig
+    {
+        public static Saml2Configuration Saml2Configuration { get; private set; } = new Saml2Configuration();
+
+        public static void RegisterIdentity()
+        {
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+
+            Saml2Configuration.Issuer = ConfigurationManager.AppSettings["Saml2:Issuer"];
+            Saml2Configuration.SignatureAlgorithm = ConfigurationManager.AppSettings["Saml2:SignatureAlgorithm"];
+            Saml2Configuration.SignatureValidationCertificates.Add(CertificateUtil.Load(HttpContext.Current.Server.MapPath(ConfigurationManager.AppSettings["Saml2:SignatureValidationCertificate"])));
+            Saml2Configuration.CertificateValidationMode = (X509CertificateValidationMode)Enum.Parse(typeof(X509CertificateValidationMode), ConfigurationManager.AppSettings["Saml2:CertificateValidationMode"]);
+            Saml2Configuration.RevocationMode = (X509RevocationMode)Enum.Parse(typeof(X509RevocationMode), ConfigurationManager.AppSettings["Saml2:RevocationMode"]);
+            Saml2Configuration.AllowedAudienceUris.Add(Saml2Configuration.Issuer);
+            Saml2Configuration.SingleSignOnDestination = new Uri(ConfigurationManager.AppSettings["Saml2:SingleSignOnDestination"]);
+            Saml2Configuration.SingleLogoutDestination = new Uri(ConfigurationManager.AppSettings["Saml2:SingleLogoutDestination"]);
+            Saml2Configuration.SignatureValidationCertificates.Add(CertificateUtil.Load(HttpContext.Current.Server.MapPath(ConfigurationManager.AppSettings["Saml2:SignatureValidationCertificate"])));
+        }
+    }
+}
---
+++ Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using ASOnlineSAMLWebApp.App_Start;
+using ITfoxtec.Identity.Saml2;
+using ITfoxtec.Identity.Saml2.Mvc;
+using ITfoxtec.Identity.Saml2.Schemas;
+using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Security.Claims;
+using System.Web.Mvc;
+using ASOnlineSAMLWebApp.Identity;
+
+namespace ASOnlineSAMLWebApp.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        const string relayStateReturnUrl = "ReturnUrl";
+        private readonly Saml2Configuration config;
+        public AuthController()
+        {
+            config = IdentityConfig.Saml2Configuration;
+        }
+        public ActionResult Login(string returnUrl = null)
+        {
+            var binding = new Saml2RedirectBinding();
+            binding.SetRelayStateQuery(new Dictionary<string, string> { { relayStateReturnUrl, returnUrl ?? Url.Content("~/") } });
+
+            return binding.Bind(new Saml2AuthnRequest(config){ }).ToActionResult();
+        }
+        public ActionResult AssertionConsumerService()
+        {
+            var binding = new Saml2PostBinding();
+            var saml2AuthnResponse = new Saml2AuthnResponse(config);
+
+            binding.ReadSamlResponse(Request.ToGenericHttpRequest(), saml2AuthnResponse);
+            if (saml2AuthnResponse.Status != Saml2StatusCodes.Success)
+            {
+                throw new AuthenticationException($"SAML Response status: {saml2AuthnResponse.Status}");
+            }
+            binding.Unbind(Request.ToGenericHttpRequest(), saml2AuthnResponse);
+            saml2AuthnResponse.CreateSession(claimsAuthenticationManager: new DefaultClaimsAuthenticationManager());
+
+            var relayStateQuery = binding.GetRelayStateQuery();
+            var returnUrl = relayStateQuery.ContainsKey(relayStateReturnUrl) ? relayStateQuery[relayStateReturnUrl] : Url.Content("~/");
+            return Redirect(returnUrl);
+        }
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Redirect(Url.Content("~/"));
+            }
+
+            var binding = new Saml2PostBinding();
+            var logoutRequest = new Saml2LogoutRequest(config, ClaimsPrincipal.Current).DeleteSession();
+            return binding.Bind(logoutRequest).ToActionResult();
+        }
+    }
+}
